398aa5ca622ee6f096f9904be9552e739af7c218
GabCh15 <info@thedac.info>
2022-11-15 09:18:04 -0500

    Build change

--- build/index.js
+++ build/index.js
@@ -3,20 +3,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const http_1 = __importDefault(require("http"));
-const socket_io_1 = require("socket.io");
 const socketUtils_1 = require("./lib/utils/socketUtils");
 const socketMessagesController_1 = require("./src/controller/socketMessagesController");
-require("./src/process/metaverseProcess");
-//import httpProxy from 'http-proxy'
-const app = (0, express_1.default)();
-const server = http_1.default.createServer(app);
-const io = new socket_io_1.Server(server, {
-    path: '/heatmap-backend',
-    transports: ['websocket', 'polling'],
+const cors_1 = __importDefault(require("cors"));
+const app = require('express')();
+app.use((0, cors_1.default)());
+const server = require('http').createServer(app);
+const Server = require('socket.io').Server;
+const port = process.env.PORT || 8080;
+const io = new Server(server, {
+    path: '/api/socket.io',
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST'],
+    },
 });
-const port = process.env.PORT || 3005;
 io.on('connection', (socket) => {
     console.log('Connection');
     (0, socketUtils_1.defineHandlers)(socket, (0, socketMessagesController_1.socketMessagesController)(socket));
@@ -24,4 +25,14 @@ io.on('connection', (socket) => {
 server.listen(port, () => {
     console.log('Sockets listening on port: ' + port);
 });
-server.on('upgrade', (request, socket, head) => { });
+server.on('upgrade', (req, socket, head) => {
+    console.log('beautiful upgrade');
+    if (req.url.indexOf('/socket.io') != -1) {
+        console.log('socket upgrades');
+        //io.engine.handleUpgrade(req, socket, head)
+    }
+    else {
+        console.log('destroyed');
+        socket.destroy();
+    }
+});
