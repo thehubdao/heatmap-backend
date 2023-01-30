a95e95a949de087a492d4cf34e17eff1e09d3bd3
GabCh15 <64225598+GabCh15@users.noreply.github.com>
2023-01-30 11:50:54 -0500

    Fix console logs and remove axie-infinity

--- lib/utils/metaverseUtils.ts
+++ lib/utils/metaverseUtils.ts
@@ -10,31 +10,25 @@ export const heatmapMvLandsPerRequest = {
     decentraland: {
         lands: 1200,
     },
-    'axie-infinity': {
+/*     'axie-infinity': {
         lands: 1200,
-    },
+    }, */
     'somnium-space': {
         lands: 200,
     },
 }
 
-export const getMetaverseAddress = (metaverse: Metaverse) => {
-    switch (metaverse) {
-        case 'sandbox':
-            return '0x5cc5b05a8a13e3fbdb0bb9fccd98d38e50f90c38'
-        case 'decentraland':
-            return '0xF87E31492Faf9A91B02Ee0dEAAd50d51d56D5d4d'
-        case 'somnium-space':
-            return '0x913ae503153d9A335398D0785Ba60A2d63dDB4e2'
-        default:
-            return 'None'
-    }
+export const metaverseAddress ={
+    sandbox:'0x5cc5b05a8a13e3fbdb0bb9fccd98d38e50f90c38',
+    decentraland: '0xF87E31492Faf9A91B02Ee0dEAAd50d51d56D5d4d',
+    'somnium-space': '0x913ae503153d9A335398D0785Ba60A2d63dDB4e2'
 }
+
 
 export const metaverseUrl = (metaverse: Metaverse) => {
     const urls = {
         'somnium-space': `${process.env.SOMNIUM_URL}/map`,
-        'axie-infinity': `${process.env.AXIE_URL}/requestMap`,
+/*         'axie-infinity': `${process.env.AXIE_URL}/requestMap`, */
         decentraland: `${process.env.DECENTRALAND_URL}/map`,
         sandbox: `${process.env.SANDBOX_URL}/map`,
     }
