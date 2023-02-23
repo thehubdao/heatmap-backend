8c369f38bdbd09b86f2dd847dcdd1cc0aaa2b9a1
GabCh15 <gabch15@hotmail.com>
2023-02-23 16:48:55 -0500

    parsed key

--- lib/firebaseService.ts
+++ lib/firebaseService.ts
@@ -5,17 +5,19 @@ import { Metaverse } from '../types/metaverse'
 
 config()
 
+console.log(process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/gm, "\n"))
+
 var serviceAccount:any = {
     "type": "service_account",
-    "project_id": process.env.FIREBASE_PROJECT_ID,
-    "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
-    "private_key": process.env.FIREBASE_PRIVATE_KEY,
-    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
-    "client_id": process.env.FIREBASE_CLIENT_ID,
+    "project_id": `${process.env.FIREBASE_PROJECT_ID}`,
+    "private_key_id": `${process.env.FIREBASE_PRIVATE_KEY_ID}`,
+    "private_key": `${process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/gm, "\n")}`,
+    "client_email": `${process.env.FIREBASE_CLIENT_EMAIL}`,
+    "client_id": `${process.env.FIREBASE_CLIENT_ID}`,
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
     "token_uri": "https://oauth2.googleapis.com/token",
     "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": process.env.FIREBASE_CLIENT_CERT_URL
+    "client_x509_cert_url": `${process.env.FIREBASE_CLIENT_CERT_URL}`
   }
 
 const firebaseInstance = admin.initializeApp({
