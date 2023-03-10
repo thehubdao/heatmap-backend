e7a4e8a2bc3cad901127e4a559fd8f58e95532f4
GabCh15 <gabch15@hotmail.com>
2023-03-09 19:57:59 -0500

    Fix upgrade

--- index.ts
+++ index.ts
@@ -9,76 +9,85 @@ import { ProcessMessages } from './types/process'
 import { config } from 'dotenv'
 import { Metaverse } from './types/metaverse'
 import { join } from 'path'
-import { WebSocketServer } from 'ws';
+import WebSocket from 'ws';
+const http = require('http');
 
 config()
 
 const PORT = process.env.PORT as string
 
-const wss = new WebSocketServer({ port: Number(PORT)});
+const server = http.createServer(function(req: any, res: any) {
+
+});
+
+const wss = new WebSocket.Server({ server });
+
+server.listen(PORT);
+
+/* const wss = new WebSocketServer({ port: Number(PORT)}); */
 
 wss.on(socketReceiverMessages.socketConnect, function connection(ws) {
-    clientConnect(ws)
-    ws.on('error', console.error);
+  clientConnect(ws)
+  ws.on('error', console.error);
 
-    ws.on('message', (receivedData: any) => {
-        const parsedData = receivedData.toString()
+  ws.on('message', (receivedData: any) => {
+    const parsedData = receivedData.toString()
 
-        const [message, messageData] = parsedData.split('|')
+    const [message, messageData] = parsedData.split('|')
 
-        onMessage(ws, message, messageData)
+    onMessage(ws, message, messageData)
 
-    });
+  });
 });
 
 
 const child = fork(
-    join(__dirname, '/src/process/downloadMetaverseProcess'), ['node --max-old-space-size=8192 build/index.js']
+  join(__dirname, '/src/process/downloadMetaverseProcess'), ['node --max-old-space-size=8192 build/index.js']
 )
 
 
 const processMessages: any = {
-    [ProcessMessages.newMetaverseChunk]({ chunk, metaverse }: any) {
-        setLands(chunk, metaverse)
-    },
-    [ProcessMessages.getCacheKey]({ tokenId, metaverse }: any) {
-        const cacheValue = getLand(tokenId, metaverse)
-        sendChildMessage(ProcessMessages.sendCacheKey, cacheValue)
-    },
-    [ProcessMessages.setCacheKey]({ land, metaverse }: any) {
-        setLand(land, metaverse)
-    },
-    [ProcessMessages.setMetaverseCalcs](metaverse: Metaverse) {
-        setMetaverseCalcs(metaverse)
-    }
+  [ProcessMessages.newMetaverseChunk]({ chunk, metaverse }: any) {
+    setLands(chunk, metaverse)
+  },
+  [ProcessMessages.getCacheKey]({ tokenId, metaverse }: any) {
+    const cacheValue = getLand(tokenId, metaverse)
+    sendChildMessage(ProcessMessages.sendCacheKey, cacheValue)
+  },
+  [ProcessMessages.setCacheKey]({ land, metaverse }: any) {
+    setLand(land, metaverse)
+  },
+  [ProcessMessages.setMetaverseCalcs](metaverse: Metaverse) {
+    setMetaverseCalcs(metaverse)
+  }
 }
 
 const sendChildMessage = (message: any, data?: any) => {
-    child.send({ message, data })
+  child.send({ message, data })
 }
 
 const downloadStart = () => {
-    sendChildMessage(ProcessMessages.downloadStart)
+  sendChildMessage(ProcessMessages.downloadStart)
 }
 
 child.on('message', async ({ message, data }: any) => {
-    /* pidusage(child.pid, function (err:any, stats:any) {
-        console.log(message,err,stats, new Date().toISOString());
-        
-        }); */
-    const messageHandler = processMessages[message]
-    if (!messageHandler) return
-    await messageHandler(data)
+  /* pidusage(child.pid, function (err:any, stats:any) {
+      console.log(message,err,stats, new Date().toISOString());
+      
+      }); */
+  const messageHandler = processMessages[message]
+  if (!messageHandler) return
+  await messageHandler(data)
 })
 
 child.on('error', (err) => {
-    console.log(err, child.exitCode)
-    child.disconnect()
+  console.log(err, child.exitCode)
+  child.disconnect()
 })
 
 child.on('exit', (err) => {
-    console.log(err, child.exitCode)
-    child.disconnect()
+  console.log(err, child.exitCode)
+  child.disconnect()
 })
 
 
